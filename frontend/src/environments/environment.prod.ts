4a6d2e72e5b896eabd32d4ad2d5595aa0b1b10cd
joel tamayo <jueltamayo25@gmail.com>
2025-04-15 01:06:11 -0600

    Configuracion despliegue frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://tu-backend-en-produccion.com/api'
+    apiUrl: 'https://catalogo-peliculas-desplegable-production.up.railway.app/api'
 };  
